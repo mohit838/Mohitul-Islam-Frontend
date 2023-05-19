99324a10f66e98687571a46df76523e5dd22dc9f
Mohammad Mohitul Islam <mohitul449@gmail.com>
2023-05-19 12:01:49 +0600

    Select Final Version

--- client/src/router/AllRoutes.js
+++ client/src/router/AllRoutes.js
@@ -1,15 +1,15 @@
 import React from "react";
-import LightProfessionalAnimation from "../views/all-home-version/LightProfessionalAnimation";
 import NotFound from "../views/NotFound";
 import { Routes, Route } from "react-router-dom";
 import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
+import HomeLightSidebarTwo from "../views/all-home-version/HomeLightProfessional2";
 
 const AllRoutes = () => {
   return (
     <>
       <ScrollTopBehaviour />
       <Routes>
-        <Route path="/" element={<LightProfessionalAnimation />} />
+        <Route path="/" element={<HomeLightSidebarTwo />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
