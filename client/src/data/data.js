fb3baacf08c36f54f0c1e86d3399dafd87b6cd33
mohit838 <mohitul449@gmail.com>
2023-06-02 16:43:16 +0600

    Create Dummy Data FIle

---
+++ client/src/data/data.js
@@ -0,0 +1,55 @@
+export const arrOfNavMenus = [
+  "home",
+  "about",
+  "resume",
+  "work",
+  "blog",
+  "contactus",
+];
+
+export const arrOfNavMenusLinks = {
+  home: "home",
+  about: "about",
+  resume: "resume",
+  work: "work",
+  blog: "blog",
+  contactus: "contactus",
+};
+
+export const sliderContent = {
+  name: "Mohitul Islam",
+  description: `I design and develop services for customers of all sizes,
+    specializing in creating stylish, modern websites, web
+    services and online stores.`,
+  btnText: "HIRE ME",
+  btnText2: "My Work",
+};
+
+export const sliderInfo = {
+  id: 1,
+  smallHeadTitle: "Hello there...",
+  loopText: [
+    {
+      id: 1,
+      title: "Software Developer",
+    },
+    {
+      id: 2,
+      title: "Designer",
+    },
+    {
+      id: 3,
+      title: "Photographer",
+    },
+    {
+      id: 4,
+      title: "Test Title",
+    },
+  ],
+};
+
+export const profileImg = {
+  id: 1,
+  imgSrc: "img/slider/home-professional-2.png",
+  altTag: "Mohitul Islam",
+};
