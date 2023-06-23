74ebf5ab40d01cd6469b0e66830280cfad104b7f
mohit838 <mohitul449@gmail.com>
2023-06-23 18:26:00 +0600

    working with hero section

--- client/src/components/HomePage/HeroContent/HeroContent.jsx
+++ client/src/components/HomePage/HeroContent/HeroContent.jsx
@@ -1,11 +1,34 @@
-import Button from '@/components/Common/Buttons/Button';
+import Image from 'next/image';
 
 const HeroContent = () => {
   return (
-    <div>
-      <Button>fhr</Button>
+    <div className="flex flex-wrap items-center justify-center gap-5">
+      {/* Profile Image */}
+      <div>
+        <div className={'image-container'}>
+          <Image
+            src={`${profileImg?.imgSrc}`}
+            alt="mohitul-islam"
+            fill
+            priority
+            className={'image'}
+          />
+        </div>
+      </div>
+
+      {/* Profile Info */}
+      <div>
+        <h1>
+          Hi, I'm Mohitul Islam <br />
+          and Junior Frontend React Developer.
+        </h1>
+      </div>
     </div>
   );
 };
 
 export default HeroContent;
+
+const profileImg = {
+  imgSrc: '/profile/mohit.jpeg',
+};
