5ab982e0d10dd16b86021edbb62779558cd72032
mohit838 <mohitul449@gmail.com>
2023-06-24 14:14:00 +0600

    Update work cmt

--- client/src/components/HomePage/Work/Work.jsx
+++ client/src/components/HomePage/Work/Work.jsx
@@ -2,10 +2,11 @@ import SectionTitle from '@/components/Common/Layouts/SectionTitle';
 
 const Work = () => {
   return (
-    <div id="#work" className='globalMarginBottom'>
+    <div id="#work" className="globalMarginBottom">
+      {/* Section Title and Paragraph */}
       <SectionTitle title={'Work'} />
 
-
+      {/* Main Work Content */}
     </div>
   );
 };
