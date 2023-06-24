6d905df5adee42a983db0e62dbdc57fb4f89a6a8
mohit838 <mohitul449@gmail.com>
2023-06-24 16:08:55 +0600

    create dummy json

--- src/components/HomePage/Work/Work.jsx
+++ src/components/HomePage/Work/Work.jsx
@@ -7,8 +7,232 @@ const Work = () => {
       <SectionTitle title={'Work'} />
 
       {/* Main Work Content */}
+
+      {/* Tab Menu Section */}
+      <div>Tabs</div>
+
+      {/* Items Card */}
+      <div>Items Card</div>
     </div>
   );
 };
 
 export default Work;
+
+const tabs = [
+  {
+    id: 1,
+    name: 'Web Development',
+    types: 'web',
+    active: true,
+    content: [
+      {
+        id: 1,
+        title: 'Web Dev 1',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 2,
+        title: 'Web Dev 2',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 3,
+        title: 'Web Dev 3',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 4,
+        title: 'Web Dev 4',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 5,
+        title: 'Web Dev 5',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Apps Development',
+    types: 'apps',
+    active: true,
+    content: [
+      {
+        id: 1,
+        title: 'Apps Dev 1',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 2,
+        title: 'Apps Dev 2',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 3,
+        title: 'Apps Dev 3',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 4,
+        title: 'Apps Dev 4',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 5,
+        title: 'Apps Dev 5',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Marketing',
+    types: 'marketing',
+    active: true,
+    content: [
+      {
+        id: 1,
+        title: 'Marketing Dev 1',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 2,
+        title: 'Marketing Dev 2',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 3,
+        title: 'Marketing Dev 3',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 4,
+        title: 'Marketing Dev 4',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 5,
+        title: 'Marketing Dev 5',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Grphics',
+    types: 'graphics',
+    active: true,
+    content: [
+      {
+        id: 1,
+        title: 'Grphics Dev 1',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 2,
+        title: 'Grphics Dev 2',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 3,
+        title: 'Grphics Dev 3',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 4,
+        title: 'Grphics Dev 4',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 5,
+        title: 'Grphics Dev 5',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Game Development',
+    types: 'game',
+    active: true,
+    content: [
+      {
+        id: 1,
+        title: 'Game Dev 1',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 2,
+        title: 'Game Dev 2',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 3,
+        title: 'Game Dev 3',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 4,
+        title: 'Game Dev 4',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+      {
+        id: 5,
+        title: 'Game Dev 5',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        github: '/',
+        live: '/',
+      },
+    ],
+  },
+];
