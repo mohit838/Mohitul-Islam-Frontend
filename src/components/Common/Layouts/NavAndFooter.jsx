6ec03f83902e3d0da49f7d50e3bdaa68152e5510
mohit838 <mohitul449@gmail.com>
2023-06-25 21:58:38 +0600

    Dark Mode Done

--- src/components/Common/Layouts/NavAndFooter.jsx
+++ src/components/Common/Layouts/NavAndFooter.jsx
@@ -7,18 +7,31 @@ import Navbar from './Navbar';
 
 const NavAndFooter = ({ children }) => {
   const { themes, setTheme } = useTheme();
+  const [showMode, setShowMode] = useState(true);
   const [openMenu, setOpenMenu] = useState(false);
 
   const handleMobileMenu = () => {
     setOpenMenu(!openMenu);
   };
+
+  const handleLightMode = () => {
+    setTheme('light');
+    setShowMode(true);
+  };
+  const handleDarkMode = () => {
+    setTheme('dark');
+    setShowMode(false);
+  };
+
   return (
     <>
       <ContainerWrapper>
         <Navbar
           humburger={handleMobileMenu}
           openMenu={openMenu}
-          setTheme={setTheme}
+          handleLightMode={handleLightMode}
+          handleDarkMode={handleDarkMode}
+          showMode={showMode}
         />
         {children}
       </ContainerWrapper>
