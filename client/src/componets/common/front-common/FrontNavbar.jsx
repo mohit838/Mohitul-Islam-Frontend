cc7cb624607f5a8f5c320ce300c3ffd12fefa53d
mohit838 <mohitul449@gmail.com>
2023-09-30 15:36:51 +0600

    Navbar Scratch

--- client/src/componets/common/front-common/FrontNavbar.jsx
+++ client/src/componets/common/front-common/FrontNavbar.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
 const FrontNavbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <section className='hero-section-nav'>
+      {/* logo */}
+      <div>
+        <img
+        className='mohitul-islam-logo'
+          src='https://placeholder.com/80x60'
+          alt='mohitul-islam-logo'
+        />
+      </div>
+
+      {/* Navmenu */}
+      <nav>
+        <ul className='main-nav-menu'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Servies</li>
+          <li>Gallary</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+
+      {/* Chat Button */}
+      <div>
+        <button>Let's Chat</button>
+      </div>
+    </section>
+  );
 };
 
 export default FrontNavbar;
