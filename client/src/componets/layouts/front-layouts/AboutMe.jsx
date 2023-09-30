324e1c3a170a7a2fea9c67548e91f4dee1ad4555
mohit838 <mohitul449@gmail.com>
2023-09-30 19:43:49 +0600

    About me scratch

--- client/src/componets/layouts/front-layouts/AboutMe.jsx
+++ client/src/componets/layouts/front-layouts/AboutMe.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
-  )
-}
+    <section className='about-me-section'>
+      {/* About Me Image */}
+      <div className='about-me-image w-1/2'>
+        <img
+          src='/images/mohit.jpg'
+          alt='mohitul_islam'
+        />
+      </div>
 
-export default AboutMe
+      {/* About me info */}
+      <div className='about-me-info w-1/2'>
+        {/* Info section */}
+        <div className=''>
+          <span>ABOUT ME</span>
+          <h2>
+            <span>20 Year’s Experience</span>
+            on Product Design
+          </h2>
+          <p>
+            Hello there! I'm Robert Junior. I specialize in web design and
+            development, and I'm deeply passionate and committed to my craft.
+            With 20 years of experience as a professional graphic designer
+          </p>
+        </div>
+
+        {/* Progress bar */}
+        <div className=''>
+          <div>
+            <h3>React</h3>
+            progressBar
+          </div>
+          <div>
+            <h3>React</h3>
+            progressBar
+          </div>
+          <div>
+            <h3>React</h3>
+            progressBar
+          </div>
+          <div>
+            <h3>React</h3>
+            progressBar
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
