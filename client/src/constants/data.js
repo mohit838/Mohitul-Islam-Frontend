d7eb2e5f9ae7dc6d899123791c5b97ded5b16872
mohit838 <mohitul449@gmail.com>
2023-10-01 20:10:51 +0600

    about me done

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -36,27 +36,43 @@ export const brandName = [
 
 export const skills = [
   {
+    id: 5,
+    skillName: 'JavaScript',
+    percentage: '90%',
+    code: 9,
+  },
+  {
     id: 1,
     skillName: 'React',
     percentage: '90%',
-    code: 1,
+    code: 9,
   },
   {
     id: 2,
     skillName: 'Next',
     percentage: '80%',
-    code: 2,
+    code: 8,
   },
   {
     id: 3,
     skillName: 'Node',
     percentage: '70%',
-    code: 3,
+    code: 7,
   },
   {
     id: 4,
-    skillName: 'JavaScript',
-    percentage: '95%',
-    code: 4,
+    skillName: 'TypeScript',
+    percentage: '70%',
+    code: 9,
   },
 ];
+
+// About me
+
+export const aboutMeInfo = {
+  id: 1,
+  exp: '3 Year’s Experience',
+  designation: 'on Frontend Engineering',
+  aboutMe:
+    ' I possess three years of professional experiences in the software industry, with one year of dedicated employment in this field. During my tenure, I have actively contributed to numerous projects, some of which have been successfully deployed on the web. I am currently employed at a software company, where I continue to grow and contribute to the industry.',
+};
