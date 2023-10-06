39c99892a616c39f5ee2253ae82ff92bed2e0c5d
mohit838 <mohitul449@gmail.com>
2023-10-06 10:32:26 +0600

    added prev protfolio

---
+++ backups/src/component/Features/Features-Api.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: 1,
+
+    image: "https://img.icons8.com/ios/80/000000/cloud-network.png",
+    title: "Web Applications",
+    desc: "I throw myself down among the tall grass by the stream as I lie close to the earth.",
+  },
+  {
+    id: 2,
+    image: "https://img.icons8.com/dotty/80/000000/code--v2.png",
+    title: "PSD to HTML and CSS",
+    desc: "I throw myself down among the tall grass by the stream as I lie close to the earth.",
+  },
+  {
+    id: 3,
+    image: "https://img.icons8.com/dotty/80/000000/web-design.png",
+    title: "Landing Page",
+    desc: "There are many variations of passages of Lorem Ipsum	available, but the majority.",
+  },
+];
+export default data;
---
+++ backups/src/component/Resume/ResumeApi.js
@@ -0,0 +1,44 @@
+const ResumeApi = [
+  {
+    id: 1,
+    category: "education",
+    year: "Green University of Bangladesh (2017 - 2021)",
+    title: "Computer Science and Engineering",
+    desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "3.81/4",
+  },
+  {
+    id: 2,
+    category: "education",
+    year: "Savar Cant. Public School (2009 - 2011) ",
+    title: "Higher Secondary Certificate",
+    desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "4.30/5",
+  },
+  {
+    id: 3,
+    category: "education",
+    year: "Savar Cant. Baord High School (2007 - 2009) ",
+    title: "Secondary School Certificate",
+    desc: "If you are going to use a passage. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "5/5 ",
+  },
+  {
+    id: 4,
+    category: "experience",
+    year: "BSc In CSE (2017 - 2021) ",
+    title: "Bachelor In Computer Science",
+    desc: "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "3.81/4 ",
+  },
+  {
+    id: 5,
+    category: "experience",
+    year: "FullStack Training (2021 - 2022) ",
+    title: "FullStack MERN Course",
+    desc: "Generate Lorem Ipsum which looks. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "4.95/5 ",
+  },
+];
+
+export default ResumeApi;
