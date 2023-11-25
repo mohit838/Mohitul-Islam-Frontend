bcf21f03ee210fe8f65a25b292672e8187ef54e0
mohit838 <mohitul449@gmail.com>
2023-11-25 14:25:16 +0600

    added all ui comp and fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Metadata } from '../types/Types';
 import './globals.css';
-import { Metadata } from './types/Types';
 
 const inter = Inter({ subsets: ['latin'] });
 
