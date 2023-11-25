02f5b1e831175ebb0b70ba7eb0c63f3a9ead1b05
mohit838 <mohitul449@gmail.com>
2023-11-25 15:31:00 +0600

    update theme toggler

--- components/layouts/ThemeToggler.tsx
+++ components/layouts/ThemeToggler.tsx
@@ -9,11 +9,9 @@ const ThemeToggler = () => {
 
   return (
     <div>
-      <Button onClick={() => setTheme('light')}>
-        <SunIcon />
-      </Button>
-      <Button onClick={() => setTheme('dark')}>
-        <MoonIcon />
+      <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       </Button>
     </div>
   );
