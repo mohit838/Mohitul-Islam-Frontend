e3cba49700a4a2577fdc2c8b278fb539e8ef5b54
mohit838 <mohitul449@gmail.com>
2024-01-13 14:43:32 +0600

    create about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Hero from '@/components/layouts/Hero';
+import About from "@/components/layouts/About";
+import Hero from "@/components/layouts/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
